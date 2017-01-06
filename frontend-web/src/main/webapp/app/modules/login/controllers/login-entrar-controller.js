ce96ddf444ad849e59c667be0ba270ca14b06584
Joao Pedro Milhome <pedro.milhome@hotmail.com>
2017-01-06 19:41:02 -0200

    Tela de Login

--- frontend-web/src/main/webapp/app/modules/login/controllers/login-entrar-controller.js
+++ frontend-web/src/main/webapp/app/modules/login/controllers/login-entrar-controller.js
@@ -4,9 +4,19 @@
         .controller('LoginEntrarController', LoginEntrarController);
 
     /* @ngInject */
-    function LoginEntrarController($scope){
+    function LoginEntrarController($scope, $timeout,$log, $q,$state, LoginService, AlertsService,Restangular){
+        var vm = this;
+        vm.logar = logar;
+        vm.user = {};
 
-        ///////////////////////////////////
-
+        function logar () {
+            LoginService.autenticar(vm.user)
+                .then(
+                function(retorno){
+                    AlertsService.success("Usuário Logado")
+                }).catch(function (retorno) {
+                    AlertsService.success(retorno.data.mensagens[0].msg)
+                });
+        }
     }
 })();
