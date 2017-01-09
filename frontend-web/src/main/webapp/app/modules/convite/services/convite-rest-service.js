7074d453d9f7e4466e3a3886358da8781affebf4
jose.duarte <jose.duarte@turismo.gov.br>
2017-01-09 14:21:06 -0200

    [update] -  Adição de funcionalidade de convites com auto complete de localidades

--- frontend-web/src/main/webapp/app/modules/convite/services/convite-rest-service.js
+++ frontend-web/src/main/webapp/app/modules/convite/services/convite-rest-service.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict';
 
     angular
@@ -6,66 +6,12 @@
         .service('ConviteRestService', ConviteRestService);
 
     /* @ngInject */
-    function ConviteRestService($q, $http){
-        var service = this;
-
-        service.obterListaConvite = obterListaConvite;
-        service.obterLocais = obterLocais;
-
-        ////////////////////////////
-        function obterLocais(dto){
-            var retorno = $q.defer();
-
-            retorno.resolve(
-                [
-                    {
-                        id: 1,
-                        local: 'CLN 312 Bloco E'
-                    },
-                    {
-                        id: 2,
-                        local: 'CLS 205'
-                    },
-                    {
-                        id: 3,
-                        local: 'Condomínio Solar de Brasília'
-                    },
-                    {
-                        id: 4,
-                        local: 'Condomínio Parque do Mirante'
-                    },
-                    {
-                        id: 5,
-                        local: 'Condomíno Santa Bárbara'
-                    },
-                    {
-                        id: 6,
-                        local: 'Condomínio São Francisco'
-                    },
-                    {
-                        id: 7,
-                        local: 'Lago Sul'
-                    }
-                ]
-            );
-
-            return retorno.promise;
-        }
-
-        function obterListaConvite(dto){
-            var retorno = $q.defer();
-            $http
-                 .get('modules/convite/data/list-convite.json')
-                 .success (function(data){
-                    retorno.resolve(data);
-                 })
-                 .error(function(){
-                     retorno.reject(alert('Não fooi possivel carregar os dados'));
-                 });
-            return retorno.promise;
-        }
-
+    function ConviteRestService(Restangular) {
+        return {
+            salvar: Restangular.one('eventos').customPOST,
+            obterLocais: Restangular.one('eventos/localidades').customGET,
+            obterRemetentes: Restangular.one('eventos/remetentes').customGET
+        };
     }
-
 
 })();
