b98932e896c46360dcee582ca5beeaa8d82220d1
antonio.jesus <antonio.jesus@turismo.gov.br>
2017-01-10 15:19:01 -0200

    Ajustes necessarios para front-end

--- frontend-web/src/main/webapp/app/modules/pessoas/controllers/pessoas-pesquisar-remetente-controller.js
+++ frontend-web/src/main/webapp/app/modules/pessoas/controllers/pessoas-pesquisar-remetente-controller.js
@@ -4,7 +4,7 @@
         .controller('PessoasPesquisarRemetenteController', PessoasPesquisarRemetenteController);
 
     /* @ngInject */
-    function PessoasPesquisarRemetenteController($scope, $timeout, $mdSidenav, $log, $http, $mdDialog, $state, $location, $anchorScroll, AlertsService, DTO, RemetenteService){
+    function PessoasPesquisarRemetenteController($scope, $timeout, $mdSidenav, $log, $http, $mdDialog, $state, $location, $anchorScroll, AlertsService, DTO){
         var vm = this;
         var _itens = [];
         vm.limpar = limpar;
@@ -17,70 +17,54 @@
         vm.title = "Pesquisar remetente";
         vm.tbResultado = false;
         vm.filtro = {
-            noRemetente: '',
-            noCargo: '',
-            noEmail: '',
-            nuTelefone: ''
+            nome: '',
+            cargo: '',
+            email: '',
+            tel: ''
         };
         function limpar (){
             vm.filtro ={};
         }
-
         function pesquisar (){
-
-            $state.params.filtro.filtros.noRemetente = vm.filtro.noRemetente;
-            $state.params.filtro.filtros.noCargo = vm.filtro.noCargo;
-            $state.params.filtro.filtros.noEmail = vm.filtro.noEmail;
-            $state.params.filtro.filtros.nuTelefone = vm.filtro.nuTelefone.replace(/[^0-9]/g,'');
-            $state.params.filtro.currentPage = 1;
-            getMoreInfinityScrollData($state.params.filtro.currentPage);
+            vm.tbResultado = true;
+            $location.hash('result-pesquisa');
+            // call $anchorScroll()
+            $anchorScroll();
         }
-
         function editar (remetente){
             $state.go('app.private.pessoas.editar-remetente', {remetente: remetente});
         }
 
         function showSalvar(){
-          return $scope.formRemetente.$invalid;
+            return $scope.formRemetente.$invalid;
         }
 
-        function getMoreInfinityScrollData(pageNumber){
-            $state.params.filtro.currentPage = pageNumber;
-
-            var promiseLoadMoreData = RemetenteService.consultarComFiltroSemLoader($state.params.filtro);
-
-            promiseLoadMoreData.then(function(data) {
-                    vm.tbResultado = true;
-
-                    $location.hash('result-pesquisa');
-
-                    vm.dto.totalResults = data.list.length;
-                    vm.dto.list = data.list;
-                    $anchorScroll();
-            });
-
-            return promiseLoadMoreData;
-        }
+        $scope.carregaLista = function(){
+            $http
+                .get('modules/pessoas/data/list-pessoas.json')
+                .success (function(data){
+                    _itens = data;
+                    vm.dto.totalResults = data.length;
+                    vm.dto.list = _itens.slice(0, vm.dto.pageSize);
+                })
+                .error(function(){
+                    alert('Não fooi possivel carregar os dados');
+                });
+        };
+        $scope.carregaLista();
 
         /*DIALOG*/
-        $scope.showConfirm = function(ev,remetente) {
+        $scope.showConfirm = function(ev) {
             // Appending dialog to document.body to cover sidenav in docs app
             var confirm = $mdDialog.confirm()
-            .title('Atenção')
-            .textContent('Tem certeza que deseja remover esse registro?')
-            .ariaLabel('Lucky day')
-            .targetEvent(ev)
-            .ok('Ok')
-            .cancel('Cancelar');
+                .title('Atenção')
+                .textContent('Tem certeza que deseja remover esse registro?')
+                .ariaLabel('Lucky day')
+                .targetEvent(ev)
+                .ok('Ok')
+                .cancel('Cancelar');
 
             $mdDialog.show(confirm).then(function() {
-                RemetenteService.excluirPorId(remetente.id).then(
-                    function (sucesso) {
-                        AlertsService.success('Remetente removido com sucesso.');
-                        var index = vm.dto.list.indexOf(remetente);
-                        vm.dto.list.splice(index,1);
-                    }
-                );
                 $scope.status = 'You decided to get rid of your debt.';
             }, function() {
                 $scope.status = 'You decided to keep your debt.';
@@ -92,7 +76,7 @@
 
             return function debounced() {
                 var context = $scope,
-                args = Array.prototype.slice.call(arguments);
+                    args = Array.prototype.slice.call(arguments);
                 $timeout.cancel(timer);
                 timer = $timeout(function() {
                     timer = undefined;
