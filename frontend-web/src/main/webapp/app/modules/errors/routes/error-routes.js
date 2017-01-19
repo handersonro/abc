6917f89654bd14df9454e8f80623af31a8cc9b28
antonio.jesus <antonio.jesus@turismo.gov.br>
2017-01-19 16:52:51 -0200

    [BRUNO] Novo módulo - Errors

--- frontend-web/src/main/webapp/app/modules/errors/routes/error-routes.js
+++ frontend-web/src/main/webapp/app/modules/errors/routes/error-routes.js
@@ -3,19 +3,19 @@
 
     angular
 
-        .module('sisagmApp.login.routes')
+        .module('sisagmApp.errors.routes')
         .config(configPublicRoutes);
 
     /* @ngInject */
     function configPublicRoutes($stateProvider) {
-        var MODULE_NAME = 'login';
+        var MODULE_NAME = 'errors';
         var MODULE_PATH = 'modules/'+MODULE_NAME+'/';
         var STATE_PATH = 'app.public.';
         $stateProvider
             .state(STATE_PATH+MODULE_NAME, {
                 url: '/'+MODULE_NAME,
                 abstract: false,
-                controller: 'LoginController',
+                controller: 'ErrorController',
                 controllerAs: 'vm',
                 templateUrl: MODULE_PATH+'views/'+MODULE_NAME+'-view.html',
                 resolve:  {
@@ -30,9 +30,9 @@
                     authorities: []
                 }
             })
-            .state(STATE_PATH+MODULE_NAME+'.entrar', {
-                url: '/entrar',
-                controller: 'LoginEntrarController',
+            .state(STATE_PATH+MODULE_NAME+'.401', {
+                url: '/401',
+                controller: 'Error401Controller',
                 controllerAs: 'vm',
                 templateUrl: MODULE_PATH+'views/error-401-view.html',
                 resolve: {
---
+++ frontend-web/src/main/webapp/app/modules/errors/services/error-service.js
@@ -0,0 +1,26 @@
+/**
+ * Created by joaopedromilhome on 06/01/17.
+ */
+/**
+ * Created by joaopedromilhome on 06/01/17.
+ */
+(function(){
+    'use strict';
+    /**
+     * @ngdoc function
+     * @name sisagmApp.autoridade.services:AutoridadeService
+     * @description # AutoridadeService Serviço para Autoridade
+     */
+    angular
+        .module('sisagmApp.login.services')
+        .factory('LoginService', LoginService);
+
+
+    /* @ngInject */
+    function LoginService(Restangular) {
+        return {
+            autenticar: Restangular.one('autenticar/logar').customPOST,
+            current: Restangular.one('autenticar/account').get
+        };
+    }
+})();
