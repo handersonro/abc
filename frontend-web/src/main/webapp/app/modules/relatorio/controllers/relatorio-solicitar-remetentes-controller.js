467f181f5cd16ea6f73e1da5cd0cbd0c7f0abc95
Filipe Neres da Silva Vitorino <filipe.vitorino@turismo.gov.br>
2017-01-26 10:39:46 -0200

    [UPDATE] relatorio de remetentes

--- frontend-web/src/main/webapp/app/modules/relatorio/controllers/relatorio-solicitar-remetentes-controller.js
+++ frontend-web/src/main/webapp/app/modules/relatorio/controllers/relatorio-solicitar-remetentes-controller.js
@@ -4,9 +4,41 @@
         .controller('RelatorioSolicitarRemetentesController', RelatorioSolicitarRemetentesController);
 
     /* @ngInject */
-    function RelatorioSolicitarRemetentesController($scope){
+    function RelatorioSolicitarRemetentesController($state, DTO,RelatorioService){
+        var vm = this;
+        vm.dto = new DTO();
+        vm.filtro = {};
+        vm.filtroPaginacao = {
+            "currentPage": "1",
+            "pageSize": "20",
+            "totalResults": "1",
+            "sortFields": "noRemetente",
+            "sortDirections": "asc",
+            "filtros": {
+            }
+        }
+        vm.relatorio ={};
 
         ///////////////////////////////////
+        vm.title = "Relatório de Remetentes";
+        vm.autoridade = "Ministro";
+
+        function gerarRelatorio() {
+
+            $state.get('app.private.relatorio.relatorio-solicitar-remetentes').filtroPaginacao = vm.filtroPaginacao;
+            $state.go('app.private.relatorio.relatorio-solicitar-remetentes');
+
+        }
+
+        ///////////////////////////////////
+        inicializar();
+        function inicializar() {
+            gerarRelatorio()
+            RelatorioService.obterRemetentes($state.current.filtroPaginacao)
+                .then(function (data) {
+                    vm.lista = data;
+                });
+        }
 
     }
 })();
