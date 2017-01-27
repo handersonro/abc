afaeda480f5ab01974e590ca92b35637a26edac6
leo.santos <leo.santos@turismo.gov.br>
2017-01-27 16:08:05 -0200

    [UPDATE] Atualizando config-dev.js

--- frontend-web/src/main/webapp/app/config-dev.js
+++ frontend-web/src/main/webapp/app/config-dev.js
@@ -1,7 +1,7 @@
 (function(){
     angular
         .module('sisagmApp')
-        .constant('baseURL','http://192.168.56.10:28080/sisagm-backend/api/')
+        .constant('baseURL','http://sturdeswildfly01:8080/sisagm/sisagm-backend/api/')
         .constant('versaoProjeto','${project.version}')
         .constant('ambiente','${ambiente.nome}')
         .config(configDev);
